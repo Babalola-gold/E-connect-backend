002911034dc4b9e757cd41a6b78c92ec294504ea
Babalola Ibukun <sibukun05@gmail.com>
2024-05-20 04:51:28 -0700

    add

--- model/user.model.js
+++ model/user.model.js
@@ -1,46 +1,48 @@
-const bcrypt = require("bcrypt");
-const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
+const mongoose = require("mongoose")
 require('dotenv').config();
 
-const uri = process.env.URI;
+const uri = process.env.URI
 
 mongoose.connect(uri)
-    .then(() => {
-        console.log("Database connected successfully");
+    
+    .then((response) => {
+        console.log("database has connected successful")
     })
     .catch((err) => {
-        console.error("Error connecting to the database:", err);
-    });
+        console.log(err)
+        console.log("There is an error in the database")
+    })
+let userSchema = new mongoose.Schema({
 
-const userSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
-});
+    password: { type: String, required: true },
+})
+
 
 userSchema.pre("save", function (next) {
-    if (!this.isModified("password")) {
-        return next();
-    }
-    bcrypt.hash(this.password, 10, (err, hash) => {
-        if (err) {
-            return next(err);
-        }
+        bcrypt.hash(this.password, 10, (err, hash) => {
+        
+        console.log(hash);
+        
         this.password = hash;
         next();
+
     });
 });
 
-userSchema.methods.comparePassword = function (password, callback) {
-    bcrypt.compare(password, this.password, (err, result) => {
-        if (err) {
-            return callback(err);
-        }
-        callback(null, result);
-    });
-};
+userSchema.methods.compare = function (password, callback) {
+    bcrypt.compare(password, this.password, (result, err) => {
+        console.log(result)
+        if (err) return callback(err);
+        console.error(err);
+    })
+}
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model(
+    "user", userSchema
+    )
 
 module.exports = User;
